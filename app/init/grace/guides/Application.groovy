8972d797054c51d28cfaaa51b4f0b1cc960155f3
Michael Yan <rain@rainboyan.com>
2024-02-11 23:10:44 +0800

    Generated by Grace 2022.1.0

---
+++ app/init/grace/guides/Application.groovy
@@ -0,0 +1,13 @@
+package grace.guides
+
+import grails.boot.Grails
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class Application {
+
+    static void main(String[] args) {
+        Grails.run(Application, args)
+    }
+
+}
---
+++ app/init/grace/guides/BootStrap.groovy
@@ -0,0 +1,11 @@
+package grace.guides
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+
+    def destroy = {
+    }
+
+}
